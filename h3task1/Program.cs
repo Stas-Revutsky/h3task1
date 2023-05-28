8472685cd1b0d83b5615ded456ce757c23d8c34e
Admin <stas.revutsky@gmail.com>
2023-05-28 23:42:25 +0300

    Add project files.

---
+++ h3task1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace h3task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine().ToLower();
+            int aoieCount = 0;
+
+            foreach (char letter in str)
+            {
+                if (letter.Equals('a') || letter.Equals('o') ||
+                    letter.Equals('i') || letter.Equals('e')) { aoieCount++; };
+            }
+            
+            Console.WriteLine($"There are {aoieCount} a o i or e letters in the text");
+        }
+    }
+}
